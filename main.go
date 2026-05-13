1426e71f085450057e8cf9a4bbcadf511be4fd57
Aleksandr Shegai <a.shegai01@gmail.com>
2026-05-13 18:55:28 +0300

    solution 2

--- main.go
+++ main.go
@@ -7,14 +7,19 @@ import (
 )
 
 func main() {
+	var (
+		magicNum3 = 3
+		magicNum2 = 2
+	)
+
 	out := os.Stdout
 
-	if len(os.Args) != 2 && len(os.Args) != 3 {
+	if len(os.Args) != magicNum2 && len(os.Args) != magicNum3 {
 		panic("usage go run main.go . [-f]")
 	}
 
 	path := os.Args[1]
-	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	printFiles := len(os.Args) == magicNum3 && os.Args[2] == "-f"
 
 	err := dirTree(out, path, printFiles)
 	if err != nil {
@@ -32,23 +37,31 @@ func buildTree(out io.Writer, path, prefix string, printFiles bool) error { //
 		notLast string = "├───"
 	)
 
-	files, err := getFile(path, printFiles)
+	files, err := getFiles(path, printFiles)
 	if err != nil {
 		return err
 	}
-	for _, f := range files {
-		if f.IsDir() {
-			fmt.Fprintf(out, prefix+last+f.Name()+"\n")
 
-			if err = buildTree(out, path+"/"+f.Name(), prefix+"\t", printFiles); err != nil {
+	var (
+		newPrefix string
+		branch    string
+	)
+
+	for i, f := range files {
+		if isLast(files, i) {
+			newPrefix = prefix + "\t"
+			branch = last
+		} else {
+			newPrefix = prefix + "│\t"
+			branch = notLast
+		}
+
+		if f.IsDir() {
+			fmt.Fprintf(out, prefix+branch+f.Name()+"\n")
+
+			if err = buildTree(out, path+"/"+f.Name(), newPrefix, printFiles); err != nil {
 				return err
 			}
-			// } else {
-			// 	fmt.Fprintf(out, prefix+notLast+f.Name()+"\n")
-			// }
-			// 	if err := buildTree(out, path+"/"+f.Name(), prefix+"│\t", printFiles); err != nil {
-			// 		return err
-			// 	}
 
 		} else {
 			sizeInfo, errSize := getSize(f)
@@ -56,17 +69,16 @@ func buildTree(out io.Writer, path, prefix string, printFiles bool) error { //
 				return errSize
 			}
 
-			// fmt.Fprintf(out, prefix+last+f.Name()+" (%s)\n", sizeInfo)
-
-			fmt.Fprintf(out, prefix+notLast+f.Name()+" (%s)\n", sizeInfo)
+			fmt.Fprintf(out, prefix+branch+f.Name()+" (%s)\n", sizeInfo)
 		}
-
 	}
+
 	return nil
 }
 
-func getFile(path string, printFiles bool) ([]os.DirEntry, error) {
+func getFiles(path string, printFiles bool) ([]os.DirEntry, error) {
 	var res []os.DirEntry
+
 	files, err := os.ReadDir(path)
 	if err != nil {
 		return nil, err
@@ -78,17 +90,20 @@ func getFile(path string, printFiles bool) ([]os.DirEntry, error) {
 		}
 	}
 
-	return nil, err
+	return res, err
 }
 
 func getSize(file os.DirEntry) (string, error) {
 	emptyFile := "empty"
 
 	info, err := file.Info()
-	if info.Size() != 0 {
-		emptyFile = fmt.Sprintf("%db", info.Size())
+	if info.Size() == 0 {
 		return emptyFile, nil
 	}
 
-	return "emptyFile", err
+	return fmt.Sprintf("%db", info.Size()), err
+}
+
+func isLast(arr []os.DirEntry, i int) bool {
+	return len(arr)-1 == i
 }
